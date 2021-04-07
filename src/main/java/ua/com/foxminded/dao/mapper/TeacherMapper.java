930610f0c1a48d1404389459c8cf5736c0540153
Oleg Shestopalov <oleg.shestopalov93@gmail.com>
2021-04-07 10:46:20 +0300

    Added TeacherMapper

---
+++ src/main/java/ua/com/foxminded/dao/mapper/TeacherMapper.java
@@ -0,0 +1,22 @@
+package ua.com.foxminded.dao.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import ua.com.foxminded.domain.entity.Teacher;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TeacherMapper implements RowMapper<Teacher> {
+
+    @Override
+    public Teacher mapRow(ResultSet resultSet, int i) throws SQLException {
+        Teacher teacher = new Teacher();
+
+        teacher.setId(resultSet.getLong("id"));
+        teacher.setName(resultSet.getString("name"));
+        teacher.setSurname(resultSet.getString("surname"));
+        teacher.setEmail(resultSet.getString("email"));
+
+        return teacher;
+    }
+}
