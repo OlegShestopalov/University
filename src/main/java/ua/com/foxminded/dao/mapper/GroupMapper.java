27f229f4da1f8c2f68f485b66f03168fc82f35be
Oleg Shestoplaov <oleg.shestopalov93@gmail.com>
2021-04-09 10:50:16 +0300

    Created Group and Student mappers

--- src/main/java/ua/com/foxminded/dao/mapper/GroupMapper.java
+++ src/main/java/ua/com/foxminded/dao/mapper/GroupMapper.java
@@ -1,19 +1,21 @@
 package ua.com.foxminded.dao.mapper;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import ua.com.foxminded.domain.entity.Group;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class GroupMapper implements RowMapper<Group> {
     @Override
     public Group mapRow(ResultSet resultSet, int i) throws SQLException {
         Group group = new Group();
         group.setId(resultSet.getLong("id"));
         group.setName(resultSet.getString("name"));
-        group.setFaculty(resultSet.getInt("faculty"));
-        group.setCourse(resultSet.getInt("course"));
+        group.setFaculty(resultSet.getInt("faculty_id"));
+        group.setCourse(resultSet.getInt("course_id"));
         return group;
     }
 }
--- src/main/java/ua/com/foxminded/dao/mapper/StudentMapper.java
+++ src/main/java/ua/com/foxminded/dao/mapper/StudentMapper.java
@@ -1,25 +1,37 @@
 package ua.com.foxminded.dao.mapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ua.com.foxminded.dao.GroupDAO;
 import ua.com.foxminded.domain.entity.Group;
 import ua.com.foxminded.domain.entity.Student;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class StudentMapper implements RowMapper<Student> {
+
+    @Autowired
+    private GroupDAO groupDAO;
+
+    public StudentMapper() {
+
+    }
 
     @Override
     public Student mapRow(ResultSet resultSet, int i) throws SQLException {
         Student student = new Student();
 
         student.setId(resultSet.getLong("id"));
-        student.setGroup(resultSet.getLong("group_id"));
+        Long groupId = resultSet.getLong("group_id");
         student.setName(resultSet.getString("name"));
         student.setSurname(resultSet.getString("surname"));
         student.setSex(resultSet.getString("sex"));
         student.setAge(resultSet.getInt("age"));
         student.setEmail(resultSet.getString("email"));
+        student.setGroup(groupDAO.findById(groupId));
 
         return student;
     }
