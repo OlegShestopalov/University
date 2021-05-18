b1f0109ae481ef64e4aee0f7a38111c2737af29b
Oleg Shestopalov <oleg.shestopalov93@gmail.com>
2021-05-18 13:22:22 +0300

    Changed configuration springMVC to springBoot

--- src/main/java/ua/com/foxminded/config/SpringConfig.java
+++ src/main/java/ua/com/foxminded/config/SpringConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -13,7 +12,6 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan("ua.com.foxminded")
 public class SpringConfig implements WebMvcConfigurer {
 
