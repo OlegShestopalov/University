d7ecd77a04ac5382f136a3f53926ecdfd80b9396
Oleg Shestopalov <oleg.shestopalov93@gmail.com>
2021-06-11 16:02:26 +0300

    Introduced some changes to sql scripts

---
+++ src/main/resources/scripts/lessons.sql
@@ -0,0 +1,6 @@
+insert into lesson (name) values ('first');
+insert into lesson (name) values ('second');
+insert into lesson (name) values ('third');
+insert into lesson (name) values ('fourth');
+insert into lesson (name) values ('fifth');
+insert into lesson (name) values ('sixth');
